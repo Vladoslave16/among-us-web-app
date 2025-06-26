69a089387373a04f7e56814b75b3d6a1b53ce91f
Vladoslave16 <gvlad0160@gmail.com>
2025-06-26 20:31:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# app.py
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import SocketIO
+import random
 
+app = Flask(name)
+app.secret_key = 'ваш_секретный_ключ'
+socketio = SocketIO(app)
+
+# Здесь будет основной код из примера (создание комнат, лобби и т.д.)
+# ...
+
+if name == 'main':
+    socketio.run(app, debug=True)
